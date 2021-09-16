9aefd5ad02050d7f2047b9c5ef9ddb9b4eef0a22
Mathieu Mattei <mattei.mathieu2b1@gmail.com>
2021-09-16 15:07:18 +0200

    4. Gestion du cache - résultat

--- main.js
+++ main.js
@@ -21,10 +21,20 @@ function loadTechnologies(technos) {
         .catch(console.error);
 }
 
+loadTechnologies(technos);
+
 if(navigator.serviceWorker) {
     navigator.serviceWorker
         .register('sw.js')
         .catch(err => console.error('service worker NON enregistré', err));
 }
 
-loadTechnologies(technos);
+// if(window.caches) {
+//     caches.open('veille-techno-1.0').then(cache => {
+//         cache.addAll([
+//             'index.html',
+//             'main.js',
+//             'vendors/bootstrap4.min.css'
+//         ]);
+//     });
+// } 
